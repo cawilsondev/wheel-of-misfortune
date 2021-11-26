d360d1e2e4848d7120ffa45ed4d3e2a852222425
Chris Wilson <christopher.wilson@strath.ac.uk>
2021-11-26 16:06:36 +0000

    progress

---
+++ js/index.js
@@ -0,0 +1,83 @@
+let canvas;
+let context;
+let wheel;
+
+const canvasDimensions = {
+    width: 1000, // px
+    height: 500 // px
+};
+const radius = canvasDimensions.width / 6;
+const dpr = window.devicePixelRatio;
+
+const init = () => {
+
+    document.addEventListener('DOMContentLoaded', () => {
+
+        canvas = document.getElementById("wheelOfMisfortune");
+        context = canvas.getContext("2d");
+
+        setCanvas(() => {
+
+            wheel = new Wheel(canvas, context, canvasDimensions, dpr, radius);
+
+            // this function is called 60 times a second
+            // and this is the thing that does it
+            requestAnimationFrame(animate);
+        });
+    });
+}
+
+const setCanvas = (done) => {
+    console.log('setCanvas() canvas', canvas);
+
+    console.log('setCanvas() dpr', dpr);
+
+    // Set display size (css pixels)
+    canvas.style.width = canvasDimensions.width + 'px';
+    canvas.style.height = canvasDimensions.height + 'px';
+
+    // Set actual size in memory (scaled to account for extra pixel density)
+    // This will prevent it from being all blurry on mobiles
+    canvas.width = Math.floor(canvasDimensions.width * dpr);
+    canvas.height = Math.floor(canvasDimensions.height * dpr);
+
+    // Normalise coordinate system to use css pixels
+    context.scale(dpr, dpr);
+
+    done();
+}
+
+const animate = () => {
+
+    requestAnimationFrame(animate);
+
+    // clear canvas
+    // so we're not just overlapping things on top of eachother at 60fps
+    // also, set translation and scaling
+    context.setTransform(dpr, 0, 0, dpr, 0, 0);
+    context.clearRect(0, 0, canvasDimensions.width, canvasDimensions.height);
+
+    // we've cleared the canvas
+    // normalise so a pixel is a pixel regardless of device pixel ratio
+    context.scale(dpr, dpr);
+
+    // set background
+    context.fillStyle = 'grey';
+
+    // fillRect(x, y, width, height)
+    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillRect
+    context.fillRect(0, 0, canvasDimensions.width * dpr, canvasDimensions.height * dpr);
+
+    // draw title
+    context.fillStyle = 'white';
+    context.font = '45px Roboto';
+    context.textAlign = 'center';
+    context.textBaseline = 'middle';
+    
+    // fillText(text, x, y [, maxWidth])
+    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillText
+    context.fillText('Wheel of Misfortune', canvasDimensions.width / 2, 50);
+
+    wheel.update();
+
+}
---
+++ js/wheel.js
@@ -0,0 +1,122 @@
+class Wheel {
+
+    timeStamp = 0;
+    touchY = 0;
+    lastY = 0;
+    angle = 0;
+
+    constructor(canvas, context, canvasDimensions, dpr, radius) {
+        this.radius = radius;
+        this.context = context;
+        this.canvasDimensions = canvasDimensions;
+        this.dpr = dpr;
+
+        this.subscribeEvents();
+    }
+
+    subscribeEvents() {
+
+        let mouseDown = false;
+        
+        canvas.addEventListener('mousedown', () => {
+            // console.log('canvas mousedown');
+            mouseDown = true;
+        });
+
+        canvas.addEventListener('mouseup', () => {
+            // console.log('canvas mouseup');
+            mouseDown = false;
+        });
+
+        canvas.addEventListener('mousemove', (event) => {
+            if (!mouseDown) return;
+            // console.log('canvas mousemove', event);
+
+            // TODO
+            // if mouse moves out the canvas while mousedown
+            // no mouseup event happens
+
+            // angle
+            this.setAngle(this.radiansToDegrees(event.clientY), this.getVelocity(event.clientY));
+        });
+    }
+
+    update() {
+        this.draw();
+    }
+
+    draw() {
+
+        // set translation and scaling before rotating context
+        context.setTransform(1, 0, 0, 1, 0, 0);
+        context.rotate(this.angle);
+
+        // create a new path
+        this.context.beginPath();
+
+        // create arc
+        this.context.arc(
+            this.canvasDimensions.width / 2, // our x start point (centre of arc)
+            this.canvasDimensions.height / 1.8, // our y start point (centre of arc)
+            this.radius, // the radius from centre of arc
+            0, // start of arc line
+            Math.PI * 1.8, // end of line (Math.PI * 2 === full circle)
+            false // draw anti-clockwise? doesn't matter in this case
+        );
+
+        // how fat do we want the stroke?
+        this.context.lineWidth = 20;
+
+        // the colour of the stroke
+        this.context.strokeStyle = 'white'
+
+        // close the path
+        this.context.closePath();
+
+        // draw it
+        this.context.stroke();
+    }
+
+    getVelocity(clientY) {
+
+        const timeNow = Date.now();
+        const currentY = clientY;
+
+        const dt = timeNow - this.timeStamp;
+        const distance = Math.abs(currentY - this.touchY);
+        const speed = distance / dt;
+
+        this.touchY = currentY;
+        this.timeStamp = timeNow;
+
+        return speed / 20;
+    }
+
+    setAngle(yRadians, velocity) {
+
+        // console.log('Wheel.setAngle() yRadians', yRadians);
+        // console.log('Wheel.setAngle() velocity', velocity);
+
+        if (yRadians < this.lastY) {
+            // console.log('Wheel.setAngle() going up');
+            // this.angle -= velocity;
+            this.angle -= 0.01;
+        }
+
+        if (yRadians > this.lastY) {
+            // console.log('Wheel.setAngle() going down');
+            // this.angle += velocity;
+            this.angle += 0.01;
+        }
+
+        this.lastY = yRadians;
+
+        // console.log('Wheel.setAngle() angle', this.angle);
+
+    }
+
+    radiansToDegrees(yPos) {
+        return yPos * Math.PI / 180;
+    }
+
+}
